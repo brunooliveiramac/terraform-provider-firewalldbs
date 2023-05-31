8cdefa385dca4d0259656bea8a71bd86d8800526
Luan Lopes <luan.lopes94@gmail.com>
2023-05-31 15:26:34 -0300

    feat(validation): add async validation for firewall update.

--- firewalldbs/data_provider/AzurePostgresClient.go
+++ firewalldbs/data_provider/AzurePostgresClient.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"time"
 	"terraform-provider-firewalldbs/firewalldbs/core"
 	"terraform-provider-firewalldbs/firewalldbs/core/entity"
 	"terraform-provider-firewalldbs/firewalldbs/data_provider/model"
@@ -62,6 +63,18 @@ func (p PostgresProvider) AddAgentIp(firewall *entity.ServerFirewallIpRule, toke
 		return errors.New(msg)
 	}
 
+	headers := resp.Header
+	asyncUrl := headers.Get("azure-asyncoperation")
+
+	check, err2 := p.CheckAgentIpAllowed(asyncUrl, token)
+	if err2 != nil {
+		return err2
+	}
+	if !check {
+		errMsg := fmt.Sprintf("Failed to add IP %s in PostgreSQL.", firewall.IP)
+		return errors.New(errMsg)
+	}
+
 	defer resp.Body.Close()
 
 	return nil
@@ -103,4 +116,61 @@ func (p PostgresProvider) DeleteAgentIp(firewall *entity.ServerFirewallIpRule, t
 
 	return nil
 
+}
+
+func (p PostgresProvider) CheckAgentIpAllowed(url string, token string) (bool, error) {
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return false, err
+	}
+
+	tokenFmt := fmt.Sprintf("Bearer %s", token)
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", tokenFmt)
+
+	client := &http.Client{}
+	var response model.AsyncResponse
+
+	startTime := time.Now()
+	endTime := startTime.Add(5 * time.Minute)
+	ticker := time.NewTicker(5 * time.Second)
+
+	for range ticker.C {
+		resp, err := client.Do(req)
+		if err != nil {
+			ticker.Stop()
+			return false, err
+		}
+
+		bodyBytes, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			ticker.Stop()
+			return false, err
+		}
+
+		err = json.Unmarshal(bodyBytes, &response)
+		if err != nil {
+			ticker.Stop()
+			return false, err
+		}
+
+		if response.Status == "Succeeded" {
+			log.Printf("IP address added successfully.")
+			ticker.Stop()
+			return true, nil
+		}
+
+		log.Printf("IP address not yet added.")
+
+		if time.Now().After(endTime) {
+			ticker.Stop()
+			break
+		}
+	}
+
+	return false, nil
 }
